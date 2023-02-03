9335f3a8aee4e01515f3b5968dea4304bacaad28
Dima-Faraji <dima.faraji@outlook.com>
2023-02-03 17:28:39 +0100

    Update task2.py
    make changes: Delete line one <from to do import TODO

--- src/lms126/task2.py
+++ src/lms126/task2.py
@@ -1,9 +1,9 @@
-from to_do import TODO
+
 def task2():
     return """
     Area of Rectangle:
     Start
-    Input Width (W) and Height (H).
+    Input Width (W) and Height (H)
     Multiply(*) Width (W) and Height (H), and store (=) it in Area (A)
     Print Area (A)
     Stop
