880f6367bb3a495e8b16e3f570f381fbb540f397
Dima-Faraji <dima.faraji@outlook.com>
2023-02-04 08:59:04 +0100

    New updated task 2
    Add description

--- src/lms126/task2.py
+++ src/lms126/task2.py
@@ -1,10 +1,14 @@
 
 def task2():
+    # Height = ("Input  Height (H)")
+    # Width = ("Input  Width (W)")
+    # Area = ("Multiply(*) Width (W) and Height (H)")
+    # Print = ("Output = Print Area (A)")
     return """
     Area of Rectangle:
     Start
     Input Width (W) and Height (H)
     Multiply(*) Width (W) and Height (H), and store (=) it in Area (A)
-    Print Area (A)
+    Output = Print Area (A)
     Stop
 """
