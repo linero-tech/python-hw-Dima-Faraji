78db24336366f65aca2ce918e81c44f1cb8a7f9a
Dima-Faraji <dima.faraji@outlook.com>
2023-02-06 18:57:33 +0100

    Update task10.py

--- src/lms126/task10.py
+++ src/lms126/task10.py
@@ -1,5 +1,24 @@
 from to_do import TODO
 
-
 def task10():
-    return TODO("Erase this line and write your solution")
+    return """
+    Start
+    Go to the website
+    Click create new email
+    INPUT email name
+        If email is uniq then precess and print ("Email name is available")
+    Else 
+    print ("Name are taken, please enter another name")
+    
+    INPUT password
+    password => six characters
+        If password => six characters then print ( "Password looks good!")
+    Else
+    Print ("You need to enter six characters long!)
+    
+    Process email and password 
+    OUTPUT print ("Email and password are created!")
+    End
+    
+    
+    """
