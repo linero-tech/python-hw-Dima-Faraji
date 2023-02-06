b5fbd75e91da9e7327e8fed08105dce6f1124d71
Dima-Faraji <dima.faraji@outlook.com>
2023-02-06 13:08:29 +0100

    Update task6.py

--- src/lms126/task6.py
+++ src/lms126/task6.py
@@ -1,5 +1,9 @@
 from to_do import TODO
 
-
 def task6():
-    return TODO("Erase this line and write your solution")
+    return """
+    Start
+    Print number (x) while x * 5 <= 100 
+    Else print "0"
+    End
+    """
