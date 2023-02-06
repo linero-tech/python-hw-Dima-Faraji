cfd9a37f18adbed47b2353af3ef99567db7df9e7
Dima-Faraji <dima.faraji@outlook.com>
2023-02-06 13:13:37 +0100

    Update task4.py

--- src/lms126/task4.py
+++ src/lms126/task4.py
@@ -1,12 +1,12 @@
 from to_do import TODO
+
 def task4():
     return """
     Start
-    Read X and Y
+    Input X and Y
     if X > Y
     Print X is grater than Y
     Else
-    If X < Y
     Print Y is greater than X
     End 
     
