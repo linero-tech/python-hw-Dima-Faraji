247ef735419056c1f2a572831445fe17545ccd35
Dima-Faraji <dima.faraji@outlook.com>
2023-02-06 18:30:28 +0100

    Update task9.py

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -1,5 +1,14 @@
 from to_do import TODO
 
-
 def task9():
-    return TODO("Erase this line and write your solution")
+    return """
+    Start
+    Program (ATM_WITHDRAWAL)
+    Print =("Enter amount to withdraw")
+    INPUT amount
+    READ balance
+    Dispense cash amount
+    balance = balance – amount
+    Print ('Your balance is’, balance)
+    End
+"""
