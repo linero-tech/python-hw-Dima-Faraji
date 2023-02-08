585fc805431af6d42ee3fdacfc2bd2c9ef023d52
Dima-Faraji <dima.faraji@outlook.com>
2023-02-08 17:28:18 +0100

    Update task5.py

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -3,23 +3,24 @@ from to_do import TODO
 def task5():
     return """
     Start
-    print(" Please Enter x from 1 to 7 (Consider 1= Monday, and 7 = Sunday)"): 
+    OUTPUT print(" Please Enter x from 1 to 7 (Consider 1= Monday, and 7 = Sunday)"): 
     Input number x
-    If x = 1 then print "Monday is 1"
-    else
-    If x = 2 then print "Tuesday is 2" 
-    else
-    If x = 3 then print "Wednesday is 3"
-    else
-    If x = 4 then print "Thursday is 4"
-    else
-    If x = 5 then print "Friday is 5"
-    else
-    If x = 6 then print "Saturday is 6"
-    else
-    If x = 7 then print "Sunday is 7"
-    else
-    If x = 8 then print "Please enter Valid Number between 1 to 7"
+    If x = 1 
+     OUTPUT print "Monday is 1"
+    else If x = 2 
+     OUTPUT print "Tuesday is 2" 
+    else If x = 3 
+     OUTPUT print "Wednesday is 3"
+    else If x = 4 
+     OUTPUT print "Thursday is 4"
+    else If x = 5
+     OUTPUT print "Friday is 5"
+    else If x = 6 
+     OUTPUT print "Saturday is 6"
+    else If x = 7 
+     OUTPUT print "Sunday is 7"
+    else If x = 8 
+     OUTPUT print "Please enter Valid Number between 1 to 7"
     End
     
 """
