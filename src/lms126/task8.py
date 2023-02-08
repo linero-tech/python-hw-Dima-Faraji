dc144d3c1a6c3286c4614c7055962182c1fcb549
Dima-Faraji <dima.faraji@outlook.com>
2023-02-08 17:53:43 +0100

    Update task8.py

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -3,9 +3,10 @@ from to_do import TODO
 
 def task8():
 
+    # For numbers which are multiples of both 3 and 5, print “FooBar” instead of the number.
+    # I should write this condition first
     # For each multiple of 3, print “Foo” instead of the number.
     # For each multiple of 5, print “Bar” instead of the number.
-    # For numbers which are multiples of both 3 and 5, print “FooBar” instead of the number.
     # Else print "Qix"
 
     return """
