a7e61bae43e8aac7858915e9cc90ad391f722252
Dima-Faraji <dima.faraji@outlook.com>
2023-02-15 18:53:10 +0100

    Update task2.py

--- src/lms127/task2.py
+++ src/lms127/task2.py
@@ -1,10 +1,13 @@
 from to_do import TODO
-name = "Dima"
-country = "Syria"
-month = "December"
-message = f"Hello {name}, you are from {country} and you were born in {month}"
-print(message)
-
 
 def task2():
-    TODO("Erase this line and write your solution")
+    name = "Dima"
+    country = "Syria"
+    month = "December"
+    message = f"Hello {name}, you are from {country} and you were born in {month}"
+    print(message)
+    return message
+
+if __name__ == "__main__":
+        task2()
+
