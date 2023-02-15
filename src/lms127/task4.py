bd6c936a562b84d3604d155e4e0ab5233f8f78c7
Dima-Faraji <dima.faraji@outlook.com>
2023-02-15 19:16:00 +0100

    Update task4.py

--- src/lms127/task4.py
+++ src/lms127/task4.py
@@ -2,7 +2,7 @@ from to_do import TODO
 
 def task4(base, height):
     result = 0.5 * (base * height)
-    print(result)
     return result
 if __name__ == "__main__":
-        task4(base=5.0,height=10.0)
+        print(task4(base=5.0,height=10.0))
+        
