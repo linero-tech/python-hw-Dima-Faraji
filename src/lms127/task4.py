4f8768a802f6a19cdf2541afc4225cda8eeb0df2
Dima-Faraji <dima.faraji@outlook.com>
2023-02-15 19:10:17 +0100

    Update task4.py

--- src/lms127/task4.py
+++ src/lms127/task4.py
@@ -1,10 +1,8 @@
 from to_do import TODO
-base = 5.0
-height = 10.0
-result = 0.5*(base*height)
-print(result)
 
 def task4(base, height):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 0.5 * (base * height)
+    print(result)
+    return result
+if __name__ == "__main__":
+        task4(base=5.0,height=10.0)
