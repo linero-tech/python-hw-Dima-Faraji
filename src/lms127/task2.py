43954e8101eed53b2c55b6faa03c1f3c2635453c
Dima-Faraji <dima.faraji@outlook.com>
2023-02-15 12:40:06 +0100

    Update task2.py

--- src/lms127/task2.py
+++ src/lms127/task2.py
@@ -1,4 +1,9 @@
 from to_do import TODO
+name = "Dima"
+country = "Sweden"
+month = "December"
+message = f"Hello {name}, you are from {country} and you were born in {month}"
+print(message)
 
 
 def task2():
