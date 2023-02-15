68bcc7ac01e30e2ff14a2ad40b5281a6050b3b13
Dima-Faraji <dima.faraji@outlook.com>
2023-02-15 13:02:42 +0100

    Update task3.py

--- src/lms127/task3.py
+++ src/lms127/task3.py
@@ -1,5 +1,14 @@
 from to_do import TODO
+# Area of Circle: It is the area occupied by the circle. Given by the formula
+# Area = π*R*R
+# Value of π = 3.14, R is the radius of a circle.
+# Perimeter of Circle: Also known as the circumference. It is given by the formula
+# Perimeter = 2*π*R
+# π = 3.14
 
+radius = 2.0
+result = (2*3.14*radius)
+print(f"result is {result}")
 
 def task3(radius):
     return TODO(
