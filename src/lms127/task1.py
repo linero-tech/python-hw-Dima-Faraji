14093f9f4295764fd5dd7f3f23299fddd541c700
Dima-Faraji <dima.faraji@outlook.com>
2023-02-15 18:48:42 +0100

    Update task1.py

--- src/lms127/task1.py
+++ src/lms127/task1.py
@@ -4,8 +4,9 @@ def task1():
     country = "Sweden"
     capital = "Stockholm"
     currency = "SEK"
-    message = f"{country}: \n {capital}: \n {currency}: "
+    message = f"Country: {country} \nCapital: {capital} \nCurrency: {currency} "
     print(f"{message.title()}")
+    return message
 
-    if __name__ != "__main__":
-        return
+if __name__ == "__main__":
+    task1()
