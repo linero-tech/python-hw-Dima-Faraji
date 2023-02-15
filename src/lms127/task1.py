a42372986ad898fabb2f522dea03fc608675ece5
Dima-Faraji <dima.faraji@outlook.com>
2023-02-15 18:27:19 +0100

    Update task1.py

--- src/lms127/task1.py
+++ src/lms127/task1.py
@@ -1,5 +1,11 @@
 from to_do import TODO
-print("Country: Sweden \nCapital: Stockholm \nCurrency: SEK")
 
 def task1():
-    print("Country: Sweden \nCapital: Stockholm \nCurrency: SEK")
+    country = "Sweden"
+    capital = "Stockholm"
+    currency = "SEK"
+    message = f"{country}: \n {capital}: \n {currency}: "
+    print(f"{message.title()}")
+
+    if __name__ == "__main__":
+        task1()
