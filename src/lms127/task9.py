749165b7bbb35f24d412a7ef5c7f7407ffdf30d7
Dima-Faraji <dima.faraji@outlook.com>
2023-02-17 18:35:55 +0100

    Update task9.py

--- src/lms127/task9.py
+++ src/lms127/task9.py
@@ -4,5 +4,6 @@ from to_do import TODO
 def task9(sentence, character):
     result = character in sentence
     return result
+
 if __name__ == "__main__":
-        print(task9("I code in KOTLIN", "i"))
+        print(task9("I code In KOTLIN", "I"))
