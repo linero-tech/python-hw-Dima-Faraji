67f9dbd56101f33595fc555ff81b3cff8009cb50
Dima-Faraji <dima.faraji@outlook.com>
2023-02-19 10:18:19 +0100

    Update task1.py

--- src/lms127/task1.py
+++ src/lms127/task1.py
@@ -2,9 +2,7 @@ from to_do import TODO
 
 # 'Country: COUNTRY NAME\nCapital: CAPITAL NAME\nCurrency: CURRENCY NAME\n'
 def task1():
-    result = "Country: Panama\nCapital: Panama City\nCurrency: USD\n"
-    return result
-
+    return "Country: Panama\nCapital: Panama City\nCurrency: USD\n"
 
 if __name__ == "__main__":
     print(task1())
