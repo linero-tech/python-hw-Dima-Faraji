5d505b25cc88801ca4c75fbc16b9d64a92578c98
Dima-Faraji <dima.faraji@outlook.com>
2023-02-20 17:32:03 +0100

    Update task1.py

--- src/lms127/task1.py
+++ src/lms127/task1.py
@@ -2,10 +2,10 @@ from to_do import TODO
 
 # 'Country: COUNTRY NAME\nCapital: CAPITAL NAME\nCurrency: CURRENCY NAME\n'
 def task1():
-    country = "United Arab Emirates"
-    capital = "Abu Dhabi"
-    currency = "AED"
-    print(f"Country: {country}\nCapital: {capital}\nCurrency: {currency}\n")
+    print("Country: United Arab Emirates")
+    print("Capital: Abu Dhabi")
+    print("Currency: AED")
+    
 
 if __name__ == "__main__":
     task1()
