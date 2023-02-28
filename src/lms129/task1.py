79aa4d26ebbb5519701fc40dc383277bcb7314af
Dima-Faraji <dima.faraji@outlook.com>
2023-02-28 22:21:57 +0100

    Update task1.py

--- src/lms129/task1.py
+++ src/lms129/task1.py
@@ -2,6 +2,14 @@ from to_do import TODO
 
 
 def task1(a, b):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = a, b
+    if a >= b:
+        print(0)
+    else:
+        print(a, b)
+    return result
+
+if __name__ == "__main__":
+    task1(1, 5)
+    task1(3, 3)
+   
