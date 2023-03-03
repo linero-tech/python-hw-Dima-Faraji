2d0d4ca89ccd80f9ddccf42a9f4af60211078a6d
Dima-Faraji <dima.faraji@outlook.com>
2023-03-03 18:05:08 +0100

    Update task3.py

--- src/lms129/task3.py
+++ src/lms129/task3.py
@@ -3,14 +3,12 @@ from to_do import TODO
 
 def task3(number):
     factorial = 1
-    if number < 0:
-        print("Sorry, factorial does not exist for negative numbers")
-    elif number == 0:
-        print("The factorial of 0 is 1")
+    if number == 0:
+        return 1
     else:
         for i in range(1, number + 1):
             factorial = factorial * i
-        return print(factorial)
+        return factorial
 
 if __name__ == "__main__":
-    task3(3)
+    print(task3(5))
