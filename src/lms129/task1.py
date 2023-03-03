a1ebf4dac21f3b4de29e6177efe49ffe9c934e8a
Dima-Faraji <dima.faraji@outlook.com>
2023-03-03 09:35:03 +0100

    Update task1.py

--- src/lms129/task1.py
+++ src/lms129/task1.py
@@ -7,11 +7,11 @@ def task1(a, b):
         if a >= b:
             result = 0
         else:
-            result = a,b
+            result = f"{a}{b}"
 
     return result
 
 
 if __name__ == "__main__":
-    print(task1(1, 5))
+    print(task1(a=1, b=5))
     print(task1(3, 3))
