fa6dc79ad96c0ffd1a7753ac1de86ae412ea2ba4
Dima-Faraji <dima.faraji@outlook.com>
2023-03-03 18:13:38 +0100

    Update task2.py

--- src/lms129/task2.py
+++ src/lms129/task2.py
@@ -2,20 +2,11 @@ from to_do import TODO
 
 
 def task2(number):
-    # Program to check if a number is prime or not
-    # define a result variable
-    result = False
-
-    if number > 1:
-        # check for factors
-        for i in range(2, number):
-            result = True
-            break
-        else:
-            result = False
-
-    return result
+    for i in range(2, number):
+        if (number % i) == 0:
+            return False
+    return True
 
 
 if __name__ == "__main__":
-    print(task2(5))
+    print(task2(9))
