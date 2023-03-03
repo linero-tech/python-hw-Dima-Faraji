322c998cfc0104acf8688aeac8a6ca7e67ce3176
Dima-Faraji <dima.faraji@outlook.com>
2023-03-03 18:01:24 +0100

    Update task2.py

--- src/lms129/task2.py
+++ src/lms129/task2.py
@@ -5,24 +5,17 @@ def task2(number):
     # Program to check if a number is prime or not
     # define a result variable
     result = False
-    if number == 1:
-        print("false")
-    elif number > 1:
+
+    if number > 1:
         # check for factors
         for i in range(2, number):
-            if (number % i) == 0:
-                # if factor is found, set result to True
-                result = True
-                # break out of loop
-                break
-        # check if result is True
-        if result:
-            print("false")
+            result = True
+            break
         else:
-            print("true")
+            result = False
 
     return result
 
 
 if __name__ == "__main__":
-    task2(5)
+    print(task2(5))
