85a23d0800b569c52e82b322df3ed26e9b9e945e
Dima-Faraji <dima.faraji@outlook.com>
2023-03-03 09:07:17 +0100

    Update task9.py

--- src/lms129/task9.py
+++ src/lms129/task9.py
@@ -2,6 +2,18 @@ from to_do import TODO
 
 
 def task9(temperature):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    temperature = float(input("Enter Temperature: "))
+    unit = input("Enter unit('C' for Celsius or 'F' for Fahrenheit): ")
+
+    if unit == 'C' or unit == 'c':
+        newTemp = 9 / 5 * temperature + 32
+        print("Temperature is", newTemp,"F")
+    elif unit == 'F' or unit == 'f':
+        newTemp = 5 / 9 * (temperature - 32)
+        print("Temperature is", newTemp,"C")
+    else:
+        print("Unknown unit", unit)
+
+
+if __name__ == "__main__":
+    (task9(30))
