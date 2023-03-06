ccc56258e2e8ae8e65d62575f97ffdd2ce7e1eb1
Dima-Faraji <dima.faraji@outlook.com>
2023-03-06 11:24:40 +0100

    Update task9.py

--- src/lms129/task9.py
+++ src/lms129/task9.py
@@ -2,19 +2,19 @@ from to_do import TODO
 
 
 def task9(temperature):
-    unit = input("Enter unit('C' for Celsius or 'F' for Fahrenheit): ")
-    newTemp = 0
-
-    if unit == 'C' or unit == 'c':
-        newTemp = f"{9 / 5 * temperature + 32}F"
-
-    elif unit == 'F' or unit == 'f':
-        newTemp = f"{5 / 9 * (temperature - 32)}C"
-
+    print("Options are: \n")
+    print("1.Convert temperatures from Celsius to Fahrenheit \n")
+    print("2.Convert temperatures from Fahrenheit to Celsius \n")
+    result = int(input("Choose any Option(1 or 2) : "))
+    if result == 1:
+        cel = float(temperature)
+        fahr = (cel * 9 / 5) + 32
+        return f"{int(fahr)}F"
     else:
-        print("Unknown unit", unit)
+        fahr = float(temperature)
+        cel = (fahr - 32) * 5 / 9
+        return f"{int(cel)}C"
 
-    return newTemp
 
 if __name__ == "__main__":
     print(task9(-30))
