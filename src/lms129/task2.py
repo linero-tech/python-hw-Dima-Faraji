6278b5b11d96ac1ac360d007bfaecb4c2a139dcd
Dima-Faraji <dima.faraji@outlook.com>
2023-03-08 11:04:21 +0100

    Update task2.py

--- src/lms129/task2.py
+++ src/lms129/task2.py
@@ -1,28 +1,12 @@
 from to_do import TODO
 
 def task2(number):
-    flag = False
 
-    if number == 1:
-        print("false")
-    elif number > 1:
-        # check for factors
-        for i in range(2, number):
-            if (number % i) == 0:
-                # if factor is found, set flag to True
-                flag = True
-                # break out of loop
-                break
-
-        # check if flag is True
-        if flag:
-            print("false")
-        else:
-            print("true")
-
+    for i in range(2, number):
+        if (number % i) == 0:
+            return False
+    return True
 
 if __name__ == "__main__":
-    task2(5)
-    task2(3)
-    task2(21)
-    task2(12)
+    print(task2(5))
+    print(task2(12))
