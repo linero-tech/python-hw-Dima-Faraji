936c44566de8cf6ade8f02e4bd7ecb32798e3f42
Dima-Faraji <dima.faraji@outlook.com>
2023-03-08 15:40:25 +0100

    Update task10.py

--- src/lms129/task10.py
+++ src/lms129/task10.py
@@ -2,29 +2,18 @@ from to_do import TODO
 import re
 
 def task10(password):
-    SpecialSym = ['$', '@', '#']
-    result = True
-
-    if len(password) < 6:
-        result = False
-
-    if len(password) > 10:
-        result = False
-
-    if not any(char.isdigit() for char in password):
-        result = False
-
-    if not any(char.isupper() for char in password):
-        result = False
-
-    if not any(char.islower() for char in password):
-        result = False
-
-    if not any(char in SpecialSym for char in password):
-        result = False
-    else:
-        return result
+    if len(password) < 6 or len(password) >= 10:
+        return False
+    if not re.search("[a-z]", password):
+        return False
+    if not re.search("[A-Z]", password):
+        return False
+    if not re.search("[0-9]", password):
+        return False
+    if not re.search("[$#@]", password):
+        return False
+    return True
 
 
 if __name__ == "__main__":
-    print(task10("DmFf1w3#@"))
+    print(task10("DmFw3679@"))
