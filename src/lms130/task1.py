73d7a5367dab62e63dc59d62a81203ba78d5034e
Dima-Faraji <dima.faraji@outlook.com>
2023-03-12 09:03:38 +0100

    Update task1.py

--- src/lms130/task1.py
+++ src/lms130/task1.py
@@ -1,9 +1,14 @@
 from to_do import TODO
 import random
 
-def task1(items):
 
-    return random.choice(items)
+def task1(items):
+    if items == []:
+        return 0
+    else:
+        return random.choice(items)
+
 
 if __name__ == "__main__":
     print(task1(items=[111, 222, 333, 444, 555]))
+    print(task1(items=[]))
