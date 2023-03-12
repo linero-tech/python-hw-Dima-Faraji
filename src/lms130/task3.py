26b9f075522a1ca87e3d4ff1843d4cd43f3e188e
Dima-Faraji <dima.faraji@outlook.com>
2023-03-12 09:52:34 +0100

    Update task3.py

--- src/lms130/task3.py
+++ src/lms130/task3.py
@@ -1,7 +1,11 @@
 from to_do import TODO
-
+import collections
 
 def task3(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+
+    return [i for i, y in collections.Counter(items).items() if y > 1]
+
+
+if __name__ == "__main__":
+    print(task3(items=[1, 1, 1, 2, 2, 3]))
+    print(task3(items=[1, 1, 1, 2, 2, 3, 3, 4, 5, 5]))
