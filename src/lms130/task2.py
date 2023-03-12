de955cccd1ddb9410027217afbe0053e25cb954c
Dima-Faraji <dima.faraji@outlook.com>
2023-03-12 09:16:51 +0100

    Update task2.py

--- src/lms130/task2.py
+++ src/lms130/task2.py
@@ -2,6 +2,13 @@ from to_do import TODO
 
 
 def task2(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    listOfZeros = [0] * (2 * len(items) - 1)
+    result = 0
+    for i in range(0, len(listOfZeros)):
+        if (i % 2 == 0):
+            listOfZeros[i] = result
+            result += 1
+    return result
+
+if __name__ == "__main__":
+    print(task2(items=[1, 2, 3, 4]))
